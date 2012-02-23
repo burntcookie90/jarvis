aaff478c1300ab7bd7e70eeb8e8bb0cf723dbadf
Westley Schrack <westleyschrack@gmail.com>
2012-02-23 18:11:41 -0500

    Added sound icons and mute icon for player.

--- js/script.js
+++ js/script.js
@@ -532,7 +532,7 @@ function mute(){
 	if(isMuted == '0')
 	{
 	$(".volume_icon").css({
-			"background-image" : "url('./css/img/play_hover.png')"
+			"background-image" : "url('./css/img/muted.png')"
 		});
 	_V_("mainvideo").volume(0); 
 	document.getElementById('volumeBar').getElementsByTagName('p')[0].style.width = ('0%');
@@ -542,7 +542,7 @@ function mute(){
 	{
 		isMuted = '0';
 		$(".volume_icon").css({
-			"background-image" : "url('./css/img/next.png')"
+			"background-image" : "url('./css/img/sound.png')"
 		});
 	_V_("mainvideo").volume(oldVolume); 
 	document.getElementById('volumeBar').getElementsByTagName('p')[0].style.width = (((oldVolume*100)+'%'));
