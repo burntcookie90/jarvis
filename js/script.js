6ef65dbd255c2fd2c2b3de8e5f74eee03c9bec18
unknown <syadav5@gatech.edu>
2012-02-26 15:38:50 -0500

    made changes to js function

--- js/script.js
+++ js/script.js
@@ -595,11 +595,14 @@ function music_video_img(div){
 		alert("it got to songs");
 				
 		}else if($(div).attr("id") == "media_select_vids") {
+			
+		alert("it got to vids");
 	
-		object= document.forms['video'];
-			object.elements["hidden"].value="false";
-			alert("it got to vids");
-	
+		object= document.forms['video'];		
+		alert("it got to the middle");
+		object.elements["hidden"].value="false";
+		alert("it got to the end");
+			
 		}
 		else if($(div).attr("id") == "media_select_img") {
 			
